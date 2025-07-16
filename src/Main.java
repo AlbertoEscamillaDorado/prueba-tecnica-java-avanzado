fa6670ffc9ae6044403aad37510367bee4eb882c
AlbertoEscamilla <alesdo2003@gmail.com>
2025-07-16 06:07:42 +0200

    ultimos retoques

--- src/Main.java
+++ src/Main.java
@@ -19,26 +19,20 @@ public class Main {
         vuelos.stream().forEach(System.out::println);
         linea();
 
-
         //Editar el valor de las fechas. Formato(Año,Mes,Dia)
         fechaDesde = LocalDate.of(2025,5,1);
         fechaHasta = LocalDate.of(2025,7,20);
         //fechaDesde = null;
         //fechaHasta = null;
 
-
-
         // Llama a la funcion de filtro
         filtrarVuelos(vuelos,Optional.ofNullable(fechaDesde),Optional.ofNullable(fechaHasta));
         linea();
-
-
     }
+
     // Funcion para mostrar un separador por consola
     public static void linea(){
         System.out.println("-----------------------------------------------------------------" +
                 "-----------------------------------------------------------------------------------");
     }
-
-
 }
